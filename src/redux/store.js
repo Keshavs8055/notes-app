fb0181c5cf29cdbe5d8576de386a9ff69cce43b0
Keshav Sharma <keshavs8055@gmail.com>
2021-08-26 17:57:55 +0530

    Removed Redux Tools

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,16 +3,5 @@ import { rootReducer } from "./rootReducer";
 
 export const store = createStore(
   rootReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ &&
-    window.__REDUX_DEVTOOLS_EXTENSION__({
-      trace: true,
-      serialize: {
-        options: {
-          undefined: true,
-          function: function (fn) {
-            return fn.toString();
-          },
-        },
-      },
-    })
+ 
 );
